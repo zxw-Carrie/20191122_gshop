59c3ec5fe5c80e26b83ac48e10b1428960e03454
zxw-Carrie <1593357304@qq.com>
2019-11-22 22:46:00 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,12 @@
+/**
+ * Created by zxw on 2019/11/20.
+ */
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+new Vue({
+    el:'#app',
+    render: h => h(App),
+    router
+})
